633156a2dc1f0fc6c462c1e665ea447afc0e31e7
golubev-og <34693908+golubev-og@users.noreply.github.com>
2018-09-05 19:45:06 +0300

    Файлы добавлены

---
+++ script.js
@@ -0,0 +1,319 @@
+window.onload = function () {
+    
+    shadow.onclick = function() {     
+        shadow.style.display = 'none';
+        i1.style.display = 'none';
+        i2.style.display = 'none';
+        i3.style.display = 'none';
+        i4.style.display = 'none';
+        i5.style.display = 'none';
+        i6.style.display = 'none';
+        i7.style.display = 'none';
+        i8.style.display = 'none';
+        i9.style.display = 'none';
+        i10.style.display = 'none';
+        i11.style.display = 'none';
+        i12.style.display = 'none';
+        i13.style.display = 'none';
+        i14.style.display = 'none';
+        i15.style.display = 'none';
+        i16.style.display = 'none';
+        i17.style.display = 'none';
+        i18.style.display = 'none';
+        i19.style.display = 'none';
+        i20.style.display = 'none';
+        i21.style.display = 'none';
+        i22.style.display = 'none';
+        i23.style.display = 'none';
+        i24.style.display = 'none';
+        i25.style.display = 'none';
+        i26.style.display = 'none';
+        i27.style.display = 'none';
+        i28.style.display = 'none';
+        i29.style.display = 'none';
+        i30.style.display = 'none';
+        i31.style.display = 'none';
+        i32.style.display = 'none';
+        i33.style.display = 'none';
+        i34.style.display = 'none';
+        i35.style.display = 'none';
+        i36.style.display = 'none';
+        i37.style.display = 'none';
+        i38.style.display = 'none';
+        i39.style.display = 'none';
+        i40.style.display = 'none';
+        i41.style.display = 'none';
+        i42.style.display = 'none';
+        i43.style.display = 'none';
+        i44.style.display = 'none';
+        i45.style.display = 'none';
+        i46.style.display = 'none';
+        i47.style.display = 'none';
+        i48.style.display = 'none';
+        i49.style.display = 'none';
+        i50.style.display = 'none';
+        i51.style.display = 'none';
+        i52.style.display = 'none';
+        i53.style.display = 'none';
+        i54.style.display = 'none';
+        i55.style.display = 'none';
+        i56.style.display = 'none';
+        i57.style.display = 'none';
+        i58.style.display = 'none';
+        i59.style.display = 'none';
+        i60.style.display = 'none';
+        i61.style.display = 'none';
+        i62.style.display = 'none';
+    }
+    
+        k1.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i1.style.display = 'block';
+    }
+        k2.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i2.style.display = 'block';
+    }
+        k3.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i3.style.display = 'block';
+    }
+        k4.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i4.style.display = 'block';
+    }
+        k5.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i5.style.display = 'block';
+    }
+        k6.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i6.style.display = 'block';
+    }
+        k7.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i7.style.display = 'block';
+    }
+        k8.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i8.style.display = 'block';
+    }
+        k9.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i9.style.display = 'block';
+    }
+        k10.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i10.style.display = 'block';
+    }
+        k11.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i11.style.display = 'block';
+    }
+        k12.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i12.style.display = 'block';
+    }
+        k13.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i13.style.display = 'block';
+    }
+        k14.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i14.style.display = 'block';
+    }
+        k15.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i15.style.display = 'block';
+    }
+        k16.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i16.style.display = 'block';
+    }
+        k17.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i17.style.display = 'block';
+    }
+        k18.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i18.style.display = 'block';
+    }
+        k19.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i19.style.display = 'block';
+    }
+        k20.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i20.style.display = 'block';
+    }
+        k21.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i21.style.display = 'block';
+    }
+        k22.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i22.style.display = 'block';
+    }
+        k23.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i23.style.display = 'block';
+    }
+        k24.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i24.style.display = 'block';
+    }
+        k25.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i25.style.display = 'block';
+    }
+        k26.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i26.style.display = 'block';
+    }
+        k27.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i27.style.display = 'block';
+    }
+        k28.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i28.style.display = 'block';
+    }
+        k29.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i29.style.display = 'block';
+    }
+        k30.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i30.style.display = 'block';
+    }
+        k31.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i31.style.display = 'block';
+    }
+        k32.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i32.style.display = 'block';
+    }
+        k33.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i33.style.display = 'block';
+    }
+        k34.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i34.style.display = 'block';
+    }
+        k35.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i35.style.display = 'block';
+    }
+        k36.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i36.style.display = 'block';
+    }
+        k37.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i37.style.display = 'block';
+    }
+        k38.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i38.style.display = 'block';
+    }
+        k39.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i39.style.display = 'block';
+    }
+        k40.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i40.style.display = 'block';
+    }
+        k41.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i41.style.display = 'block';
+    }
+        k42.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i42.style.display = 'block';
+    }
+        k43.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i43.style.display = 'block';
+    }
+        k44.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i44.style.display = 'block';
+    }
+        k45.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i45.style.display = 'block';
+    }
+        k46.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i46.style.display = 'block';
+    }
+        k47.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i47.style.display = 'block';
+    }
+        k48.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i48.style.display = 'block';
+    }
+        k49.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i49.style.display = 'block';
+    }
+        k50.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i50.style.display = 'block';
+    }
+        k51.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i51.style.display = 'block';
+    }
+        k52.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i52.style.display = 'block';
+    }
+        k53.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i53.style.display = 'block';
+    }
+        k54.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i54.style.display = 'block';
+    }
+        k55.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i55.style.display = 'block';
+    }
+        k56.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i56.style.display = 'block';
+    }
+        k57.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i57.style.display = 'block';
+    }
+        k58.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i58.style.display = 'block';
+    }
+        k59.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i59.style.display = 'block';
+    }
+        k60.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i60.style.display = 'block';
+    }
+        k61.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i61.style.display = 'block';
+    }
+        k62.onclick = function() {     
+        shadow.style.display = 'table-cell';
+        i62.style.display = 'block';
+    }
+
+    
+}
